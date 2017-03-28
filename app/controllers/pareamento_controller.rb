aee1ac4922f7261be4fcad9fefbda5cbaf89ee0d
lucimarNeves <aceleradora10@gmail.com>
2017-03-28 10:34:48 -0300

    add seeds

--- app/controllers/pareamento_controller.rb
+++ app/controllers/pareamento_controller.rb
@@ -9,7 +9,10 @@ post '/count' do
   @user = params[:user]
 
   @pairing = Pairing.find_by(student_id: @user["name_students"], pair_id: @user["name_pairs"])
-  # @pairing2 = Pairing.find_by(student_id: @user["name_pairs"], pair_id: @user["name_students"])
+
+  if @pairing == nil
+    @pairing = Pairing.find_by(student_id: @user["name_pairs"], pair_id: @user["name_students"])
+  end
 
   if @pairing == nil
     @pairing = Pairing.create(student_id: @user["name_students"], pair_id: @user["name_pairs"], count: 1)
@@ -17,30 +20,6 @@ post '/count' do
     @pairing.count += 1
     @pairing.save!
   end
-
-
-  #
-  # if @pairing == nil && @pairing2 == nil
-  #   @pairing = Pairing.create(student_id: @user["name_students"], pair_id: @user["name_pairs"], count: 1)
-  # else
-  #   if @pairing.count > @pairing2.count
-  # end
-
-
-
-# elsif @pairing != nil
-#   @pairing.count += 1
-#   @pairing.save!
-# elsif @pairing2 != nil
-#   @pairing2.count += 1
-#   @pairing2.save!
-
-
-
-  puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-  puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-  puts @pairing.inspect
-  puts "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 
   redirect '/'
 end
